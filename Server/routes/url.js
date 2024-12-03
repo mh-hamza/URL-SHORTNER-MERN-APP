aed8e4b4aed8af7e7f9bfa652c7df7b9540a19f6
Mohammad Hamza <mhdkh444@gmail.com>
2024-12-03 11:53:34 +0530

    update

--- Server/routes/url.js
+++ Server/routes/url.js
@@ -4,10 +4,13 @@ import middleware from '../middleware/middleware.js'
 
 const router = express.Router()
 
-router.post('/addUrl', middleware, urlGenrate)
-router.delete('/delete/:id', middleware, handleDelete)
-router.get('/redirect/:id', handleRedirect)
-router.get('/fetchUrl', middleware, fetchAllUrl)
-router.get('/details/:id', urlDetails)
+// router.post('/addUrl', middleware, urlGenrate)
+// router.delete('/delete/:id', middleware, handleDelete)
+// router.get('/redirect/:id', handleRedirect)
+// router.get('/fetchUrl', middleware, fetchAllUrl)
+// router.get('/details/:id', urlDetails)
+router.ger("/hello", (req, res) => {
+  res.send("Hello from Url Route")
+})
 
 export default router
