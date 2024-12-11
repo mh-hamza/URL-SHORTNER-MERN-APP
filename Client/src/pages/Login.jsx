daa81f8a71386a9127b9dfd67a0ab602ddb70444
Mohammad Hamza <mhdkh444@gmail.com>
2024-12-11 17:36:43 +0530

    update

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -61,7 +61,7 @@ function Login() {
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              className=" w-full lowercase mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             />
           </div>
           <div>
--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -77,7 +77,7 @@ function Register() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                className="w-full mt-2 lowercase px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
               />
             </div>
             <div>
